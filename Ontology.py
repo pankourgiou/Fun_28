082389176c9123ff4bc5bd34e81acd8fc7b25a80
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-02-23 05:46:10 +0200

    Update and rename Ontology.py to RandomForest.R

--- Ontology.py
+++
@@ -1,4 +0,0 @@
-from owlready2 import *
-onto_path.append("/path/to/your/local/ontology/repository")
-onto = get_ontology("http://www.lesfleursdunormal.fr/static/_downloads/pizza_onto.owl")
-onto.load()
